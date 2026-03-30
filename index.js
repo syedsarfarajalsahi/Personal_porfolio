4dd27f04a9fa803d2610f13d4a705345704e9dfe
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2026-03-30 06:59:37 +0530

    Update JS Script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const whatsappBtn = document.getElementById("whatsappBtn");
 
 whatsappBtn.addEventListener("click", function () {
-  const phoneNumber = "919876543210"; // country code + number
+  const phoneNumber = "919366100346"; // country code + number
   window.open(`https://wa.me/${phoneNumber}`, "_blank");
 });
