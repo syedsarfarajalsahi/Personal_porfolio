4f0df3035e9b31e8f31990c9dca32a02a85ed36f
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2026-03-30 06:54:17 +0530

    Update HTML for adding GitHub Link

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const whatsappBtn = document.getElementById("whatsappBtn");
 
 whatsappBtn.addEventListener("click", function () {
-  const phoneNumber = "919876543210"; // Replace with your number
+  const phoneNumber = "919366100346"; // Replace with your number
   const whatsappURL = `https://wa.me/${phoneNumber}`;
 
   window.open(whatsappURL, "_blank");
