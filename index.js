616cda520bdd2b1788ae4d927b5b739367ddab13
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2026-05-07 12:38:33 +0530

    Revert "Add Email Button"
    This reverts commit c65e6fa705418cff133abbfa889fac8fd4543ff9.

--- index.js
+++ index.js
@@ -6,17 +6,3 @@ whatsappBtn.addEventListener("click", () => {
   // Added window feature flags for additional security on redirect
   window.open(whatsappURL, "_blank", "noopener,noreferrer");
 });
-
-const emailBtn = document.getElementById("emailBtn");
-
-emailBtn.addEventListener("click", (e) => {
-  // Prevent the default anchor behavior (which would jump to the top of the page)
-  e.preventDefault(); 
-  
-  // Store the components separately
-  const user = "syedsarfarajalsahi";
-  const domain = "gmail.com";
-  
-  // Assemble and execute the OS interrupt Just-In-Time
-  window.location.href = `mailto:${user}@${domain}`;
-});
